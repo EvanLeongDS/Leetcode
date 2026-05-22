8dc2adfd41bc7cedc10e04cbb28bee5bb60a9a8d
Evan Leong <leonge1@bu.edu>
2026-05-22 19:39:01 -0400

    Time: 3157 ms (5.01%), Space: 23.6 MB (8.59%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,45 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # establish basic preprocessing
+        nums = sorted(nums)
+        print(nums)
+        n = len(nums)
+        output_list = []
+        three_set = set()
+
+        # base case of 3 
+        if n == 3:
+            total = sum(nums)
+            if total == 0:
+                output_list.append(nums)
+                return output_list
+            else:
+                return []
+        
+        # iterate through the three sum
+        i = 0
+        j = i + 1
+        k = n - 1
+        while i + 1 <= n:
+            if nums[i] > 0:
+                break
+            while j < k: # hold nums[i] nums[j] stagnant test every k
+                temp_tuple = tuple([nums[i], nums[j], nums[k]])
+                if nums[i] + nums[j] + nums[k] == 0 and temp_tuple not in three_set:
+                    output_list.append([nums[i], nums[j], nums[k]])
+                    three_set.add(temp_tuple)
+                    j += 1
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+            i += 1 
+            j = i + 1
+            k = n - 1
+
+        return output_list
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
