d220c7ceb1ee13a76b946b9d992a302a2e420ab5
Evan Leong <leonge1@bu.edu>
2026-06-12 16:19:32 -0400

    Time: 19 ms (49.09%), Space: 25.5 MB (58.42%) - LeetHub - Time: O(n log k + m log k), Space: O(k)

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,13 +1,18 @@
+import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.nums = nums
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
-        self.nums.append(val)
-        self.nums = sorted(self.nums)
-        return self.nums[-self.k]
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
         
 
 
