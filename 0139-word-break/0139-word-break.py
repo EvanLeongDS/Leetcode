0fe292d690f30651574904470055dd47ea3355b9
Evan Leong <leonge1@bu.edu>
2026-07-09 09:34:27 -0700

    Time: 8 ms (8.22%), Space: 19.3 MB (51.63%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s) + 1
+        # establish empty array and base case
+        dp = [False] * n
+        dp[0] = True # vacuously true
+
+        # build table 
+        for i in range (1, n):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+        print(dp)
+        return dp[len(s)]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
