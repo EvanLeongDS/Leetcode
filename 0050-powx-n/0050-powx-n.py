6af54aceca0978445106bcf3e98ee423b45f4a41
Evan Leong <leonge1@bu.edu>
2026-07-21 14:44:32 -0700

    Time: 0 ms (100%), Space: 19.3 MB (97.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,8 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        power = x ** n
-        print(power)
-        return power
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n == 0:
+            return 1
+        if n % 2 != 0:
+            return self.myPow(x, n - 1) * x
+        half = self.myPow(x, n//2)
+        return half * half
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
