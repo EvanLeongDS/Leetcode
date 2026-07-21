e005eeabcc22f7394736d490b04c824b34a8be47
Evan Leong <leonge1@bu.edu>
2026-07-21 14:25:44 -0700

    Time: 2 ms (20.26%), Space: 19.5 MB (54.27%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,9 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        power = x ** n
+        print(power)
+        return power
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
