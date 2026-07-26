b37c51257d52e8b5945a68ddb40211288ac77390
Evan Leong <leonge1@bu.edu>
2026-07-26 14:57:32 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,14 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res= ""
+        # Find the longest common prefix string amonst an array of strings 
+
+        prefix = ""
+        # iterate over the first string 
         for i in range(len(strs[0])):
-            matching_char = strs[0][i]
-            print(matching_char)
-            for string in strs:
-                if i == len(string) or string[i] != matching_char:
-                    return res
-            res += matching_char
-        return res
+            # iterate over all other strings 
+            for j in range(1, len(strs)):
+                # make sure the string goes that far before it goes out of bounds
+                if i > len(strs[j]):
+                    return prefix
+                # compare whether strs[0][i] is equivalent to strs[j][i]
+                if strs[0][i] != strs[j][i]:
+                    return prefix
+            prefix += strs[0][i]
+        return prefix
 
             
 
@@ -18,4 +24,4 @@ class Solution:
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
