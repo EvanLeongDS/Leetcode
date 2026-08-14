379eafbb5283d3d7402b4162b6965fea7a70beb2
Evan Leong <leonge1@bu.edu>
2026-08-14 16:10:26 -0700

    Time: 155 ms (5.04%), Space: 23.1 MB (30.52%) - LeetHub

---
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        #base case 
+        if len(arr) == 1:
+            return 1
+        max_subarray = 1 # final return variable 
+        current_subarray = 1
+        n = len(arr)
+        prev = None # tracks whether you have turbulence or not
+
+        for k in range(1, n):
+            print(current_subarray, prev)
+            if arr[k] > arr[k - 1]:
+                # add if the previous turbulent was < 
+                if prev == "<":
+                    current_subarray += 1
+                else:
+                    current_subarray = 2
+                prev = ">"
+            elif arr[k] < arr[k -1]:
+                # add if the previous turbulent was > 
+                if prev == ">":
+                    current_subarray += 1
+                else:
+                    current_subarray = 2
+                prev = "<"
+            else:
+                current_subarray = 1
+                prev = None
+            max_subarray = max(max_subarray, current_subarray)
+        return max_subarray
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
